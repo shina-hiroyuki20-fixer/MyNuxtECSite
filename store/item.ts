bc173cdd0581ec7a38d917397a0982bafb139241
shina-hiroyuki20-fixer <shina.hiroyuki20@fixer.co.jp>
2020-07-20 16:58:10 +0900

    add: Created vuex file :sparkles:
    Vuex用の.tsファイルをgettersのみ作成してみた。

---
+++ store/item.ts
@@ -0,0 +1,11 @@
+import { productsMap } from '~/products/productsMap.json';
+
+export const state = () => ({
+  itemList: productsMap
+});
+
+export const getters = {
+  findItemsByCat: state => (prodName) => {
+    return Object.values(state.itemList).filter(a => a.prodName === prodName);
+  }
+};
