0d42cfeb654c31ac01b6b034d428506a6244537e
shina-hiroyuki20-fixer <shina.hiroyuki20@fixer.co.jp>
2020-08-03 19:33:59 +0900

    add: Created cart page :sparkles:
    取り敢えず追加した。

---
+++ pages/cart/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="font-title">
+    <span style="font-style:italic;">Lorem Ipsum Express</span> | Shopping Cart
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index'
+};
+</script>
+
+<style lang="scss">
+.font-title{
+  /* Lorem Ipsum Express | Shopping Cart */
+
+  position: absolute;
+  width: 1200px;
+  height: 100px;
+  left: 360px;
+  top: 0px;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 72px;
+  line-height: 84px;
+  text-align: center;
+
+  color: #4A3C4C;
+}
+</style>
